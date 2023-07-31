ac85c41ab731a48125fff93e828793020816f356
Pratyaksh <81953991+Pratyaksh-tech@users.noreply.github.com>
2023-07-31 19:13:18 +0530

    Update AI.py

--- chess/AI.py
+++ chess/AI.py
@@ -9,9 +9,11 @@ class Handle:
 		self.bot = 'black'
 
 	def handle(self):
-		best_Score = -100000
+		best_Score = -1000000000
 		best_move = None
 		best_piece = None
+		alpha = -1000000000
+		beta = 1000000000
 		for row in range(ROWS):
 			for col in range(COLS):
 				if not self.board.chess[row][col].isEmpty_or_rival(self.bot):
@@ -22,34 +24,39 @@ class Handle:
 					for move in actual_moves:
 						opp = move.final.piece
 						self.board.make_move(piece, move)
-						
-						score = self.minimax("black", 0)
+
+						score = self.minimax("black", 0, alpha, beta)
 						self.board.undo_move(move, piece, opp)
+
 						if score > best_Score:
 							best_Score = score
 							best_move = move
 							best_piece = piece
-		print(f"best score is {best_Score} and piece is { best_piece.name}")
+
+						alpha = max(alpha, best_Score)
+						if beta <= alpha:
+							break
+
+		print(f"best score is {best_Score} and piece is {best_piece.name}")
 		return (best_move, best_piece)
 	
 	def static_evaluation(self):
 		white_score = 0
-		for row in range(ROWS):
-			for col in range(COLS):
-				if not self.board.chess[row][col].isEmpty():
-					if self.board.chess[row][col].piece.color == "white":
-						white_score += self.board.chess[row][col].piece.value
-
 		black_score = 0
+
 		for row in range(ROWS):
 			for col in range(COLS):
 				if not self.board.chess[row][col].isEmpty():
-					if self.board.chess[row][col].piece.color == "black":
-						black_score += self.board.chess[row][col].piece.value
+					piece = self.board.chess[row][col].piece
+					if piece.color == "white":
+						white_score += piece.value
+					else:
+						black_score += piece.value
+
 		return white_score - black_score
 
-	def minimax(self, next_plyr, depth):
-		if depth == 2 or self.board.is_check:
+	def minimax(self, next_plyr, depth, alpha, beta):
+		if depth == 1 or self.board.is_check:
 			return self.static_evaluation()
 
 		if next_plyr == "black":
@@ -64,10 +71,12 @@ class Handle:
 						for move in actual_moves:
 							opp = move.final.piece
 							self.board.make_move(piece, move)
-							score = self.minimax("white", depth+1)
+							score = self.minimax("white", depth + 1, alpha, beta)
 							self.board.undo_move(move, piece, opp)
-							if score > best_score:
-								best_score = score
+							best_score = max(best_score, score)
+							alpha = max(alpha, best_score)
+							if beta <= alpha:
+								break
 			return best_score
 		else:
 			best_score = 1000000000
@@ -81,9 +90,10 @@ class Handle:
 						for move in actual_moves:
 							opp = move.final.piece
 							self.board.make_move(piece, move)
-							score = self.minimax("black", depth+1)
+							score = self.minimax("black", depth + 1, alpha, beta)
 							self.board.undo_move(move, piece, opp)
-							
-							if score < best_score:
-								best_score = score			
+							best_score = min(best_score, score)
+							beta = min(beta, best_score)
+							if beta <= alpha:
+								break
 			return best_score
