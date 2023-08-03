b6af7fb1cdc2e1ef1c84e4b3edc4a3043419ca55
Pratyaksh <81953991+Pratyaksh-tech@users.noreply.github.com>
2023-08-03 17:56:16 +0530

    Update AI.py

--- chess/AI.py
+++ chess/AI.py
@@ -1,99 +1,88 @@
 from consts import *
 from game_ import Game
+import copy
 
 class Handle:
-	def __init__(self, game):
-		self.game = game
-		self.board = self.game.board
-		self.player = 'white'
-		self.bot = 'black'
+    def __init__(self, game):
+        self.game = game
+        self.board = self.game.board
+        self.player = 'white'
+        self.bot = 'black'
 
-	def handle(self):
-		best_Score = -1000000000
-		best_move = None
-		best_piece = None
-		alpha = -1000000000
-		beta = 1000000000
-		for row in range(ROWS):
-			for col in range(COLS):
-				if not self.board.chess[row][col].isEmpty_or_rival(self.bot):
-					piece = self.board.chess[row][col].piece
-					piece.moves = []
-					self.board.calc_moves(piece, row, col)
-					actual_moves = piece.moves
-					for move in actual_moves:
-						opp = move.final.piece
-						self.board.make_move(piece, move)
+    def handle(self):
+        best_score = -float('inf')
+        best_move = None
+        best_piece = None
+        for row in range(ROWS):
+            for col in range(COLS):
+                if not self.board.chess[row][col].isEmpty_or_rival(self.bot):
+                    piece = self.board.chess[row][col].piece
+                    piece.moves = []
+                    self.board.calc_moves(piece, row, col)
+                    actual_moves = piece.moves
+                    for move in actual_moves:
+                        opp = move.final.piece
+                        self.board.make_move(piece, move)
 
-						score = self.minimax("black", 0, alpha, beta)
-						self.board.undo_move(move, piece, opp)
+                        score = self.minimax(2, False)
+                        self.board.undo_move(move, piece, opp)
 
-						if score > best_Score:
-							best_Score = score
-							best_move = move
-							best_piece = piece
+                        if score > best_score:
+                            best_score = score
+                            best_move = move
+                            best_piece = piece
 
-						alpha = max(alpha, best_Score)
-						if beta <= alpha:
-							break
+        print(f"best score is {best_score} and piece is {best_piece.name}")
+        return (best_move, best_piece)
 
-		print(f"best score is {best_Score} and piece is {best_piece.name}")
-		return (best_move, best_piece)
-	
-	def static_evaluation(self):
-		white_score = 0
-		black_score = 0
+    def static_evaluation(self):
+        white_score = 0
+        black_score = 0
 
-		for row in range(ROWS):
-			for col in range(COLS):
-				if not self.board.chess[row][col].isEmpty():
-					piece = self.board.chess[row][col].piece
-					if piece.color == "white":
-						white_score += piece.value
-					else:
-						black_score += piece.value
+        for row in range(ROWS):
+            for col in range(COLS):
+                if not self.board.chess[row][col].isEmpty():
+                    piece = self.board.chess[row][col].piece
+                    if piece.color == "white":
+                        white_score += piece.value
+                    else:
+                        black_score += piece.value
 
-		return white_score - black_score
+        return black_score - white_score
 
-	def minimax(self, next_plyr, depth, alpha, beta):
-		if depth == 1 or self.board.is_check:
-			return self.static_evaluation()
+    def minimax(self, depth, maximizing_player):
+        if depth == 0:
+            return self.static_evaluation()
 
-		if next_plyr == "black":
-			best_score = -1000000000
-			for row in range(ROWS):
-				for col in range(COLS):
-					if not self.board.chess[row][col].isEmpty_or_rival(next_plyr):
-						piece = self.board.chess[row][col].piece
-						piece.moves = []
-						self.board.calc_moves(piece, row, col)
-						actual_moves = piece.moves
-						for move in actual_moves:
-							opp = move.final.piece
-							self.board.make_move(piece, move)
-							score = self.minimax("white", depth + 1, alpha, beta)
-							self.board.undo_move(move, piece, opp)
-							best_score = max(best_score, score)
-							alpha = max(alpha, best_score)
-							if beta <= alpha:
-								break
-			return best_score
-		else:
-			best_score = 1000000000
-			for row in range(ROWS):
-				for col in range(COLS):
-					if not self.board.chess[row][col].isEmpty_or_rival(next_plyr):
-						piece = self.board.chess[row][col].piece
-						piece.moves = []
-						self.board.calc_moves(piece, row, col)
-						actual_moves = piece.moves
-						for move in actual_moves:
-							opp = move.final.piece
-							self.board.make_move(piece, move)
-							score = self.minimax("black", depth + 1, alpha, beta)
-							self.board.undo_move(move, piece, opp)
-							best_score = min(best_score, score)
-							beta = min(beta, best_score)
-							if beta <= alpha:
-								break
-			return best_score
+        if maximizing_player:
+            max_eval = -float('inf')
+            for row in range(ROWS):
+                for col in range(COLS):
+                    if not self.board.chess[row][col].isEmpty_or_rival(self.bot):
+                        piece = self.board.chess[row][col].piece
+                        piece.moves = []
+                        self.board.calc_moves(piece, row, col)
+                        actual_moves = piece.moves
+                        for move in actual_moves:
+                            opp = move.final.piece
+                            self.board.make_move(piece, move)
+                            eval_ = self.minimax(depth - 1, False)
+                            self.board.undo_move(move, piece, opp)
+                            max_eval = max(max_eval, eval_)
+            return max_eval
+        else:
+            min_eval = float('inf')
+            for row in range(ROWS):
+                for col in range(COLS):
+                    if not self.board.chess[row][col].isEmpty_or_rival(self.player):
+                        piece = self.board.chess[row][col].piece
+                        piece.moves = []
+                        self.board.calc_moves(piece, row, col)
+                        actual_moves = piece.moves
+                        for move in actual_moves:
+                            opp = move.final.piece
+                            self.board.make_move(piece, move)
+                            eval_ = self.minimax(depth - 1, True)
+                            self.board.undo_move(move, piece, opp)
+                            min_eval = min(min_eval, eval_)
+            return min_eval
