0fda6a653fefc3fc680fe85eff5482bf3ab58c78
Pratyaksh <81953991+Pratyaksh-tech@users.noreply.github.com>
2023-08-04 16:02:52 +0530

    Update AI.py

--- chess/AI.py
+++ chess/AI.py
@@ -1,7 +1,7 @@
 from consts import *
 from game_ import Game
 import copy
-
+import random
 class Handle:
     def __init__(self, game):
         self.game = game
@@ -13,26 +13,31 @@ class Handle:
         best_score = -float('inf')
         best_move = None
         best_piece = None
-        for row in range(ROWS):
-            for col in range(COLS):
+        rows = [i for i in range(ROWS)]
+        random.shuffle(rows)
+        cols = [i for i in range(COLS)]
+        random.shuffle(cols)
+
+        for row in rows:
+            for col in cols:
                 if not self.board.chess[row][col].isEmpty_or_rival(self.bot):
                     piece = self.board.chess[row][col].piece
                     piece.moves = []
                     self.board.calc_moves(piece, row, col)
                     actual_moves = piece.moves
+                    random.shuffle(actual_moves)
                     for move in actual_moves:
                         opp = move.final.piece
                         self.board.make_move(piece, move)
 
-                        score = self.minimax(2, False)
-                        self.board.undo_move(move, piece, opp)
+                        score = self.minimax(2, -float('inf'), float('inf'), False)
+                        self.board.undo_move(move, piece, opp, True)
 
                         if score > best_score:
                             best_score = score
                             best_move = move
                             best_piece = piece
 
-        print(f"best score is {best_score} and piece is {best_piece.name}")
         return (best_move, best_piece)
 
     def static_evaluation(self):
@@ -50,7 +55,7 @@ class Handle:
 
         return black_score - white_score
 
-    def minimax(self, depth, maximizing_player):
+    def minimax(self, depth, alpha, beta, maximizing_player):
         if depth == 0:
             return self.static_evaluation()
 
@@ -63,12 +68,16 @@ class Handle:
                         piece.moves = []
                         self.board.calc_moves(piece, row, col)
                         actual_moves = piece.moves
+                        random.shuffle(actual_moves)
                         for move in actual_moves:
                             opp = move.final.piece
                             self.board.make_move(piece, move)
-                            eval_ = self.minimax(depth - 1, False)
-                            self.board.undo_move(move, piece, opp)
+                            eval_ = self.minimax(depth - 1, alpha, beta, False)
+                            self.board.undo_move(move, piece, opp, True)
                             max_eval = max(max_eval, eval_)
+                            alpha = max(alpha, eval_)
+                            if beta <= alpha:
+                                break
             return max_eval
         else:
             min_eval = float('inf')
@@ -79,10 +88,14 @@ class Handle:
                         piece.moves = []
                         self.board.calc_moves(piece, row, col)
                         actual_moves = piece.moves
+                        random.shuffle(actual_moves)
                         for move in actual_moves:
                             opp = move.final.piece
                             self.board.make_move(piece, move)
-                            eval_ = self.minimax(depth - 1, True)
-                            self.board.undo_move(move, piece, opp)
+                            eval_ = self.minimax(depth - 1, alpha, beta, True)
+                            self.board.undo_move(move, piece, opp, True)
                             min_eval = min(min_eval, eval_)
+                            beta = min(beta, eval_)
+                            if beta <= alpha:
+                                break
             return min_eval
